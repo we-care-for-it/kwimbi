f57e048ed459bb085558dc3ed9a7b60ec5107265
Jeff Timmer <62997572+wecareforit@users.noreply.github.com>
2024-04-09 09:54:11 +0200

    Form search page-header layout

--- resources/views/livewire/company/locations/index.blade.php
+++ resources/views/livewire/company/locations/index.blade.php
@@ -1,5 +1,12 @@
+
+
 <div>
    <div class="page-header  my-3">
+
+   
+
+
+
       <div class="row">
          <div class="col-sm-6">
             <h1 class="page-header-title pt-2">Locaties</h1>
@@ -9,22 +16,10 @@
    <div class = "filter_bar ">
    <div class="row   ">
       <div class="col-md-3">
-         <form>
-            <div class="input-group input-group-merge">
-               <input type="text" wire:model.live="filters.keyword" class="js-form-search form-control"
-                  placeholder="Zoeken op trefwoord..." data-hs-form-search-options="{
-                     &quot;clearIcon&quot;: &quot;#clearIcon2&quot;,
-                     &quot;defaultIcon&quot;: &quot;#defaultClearIconToggleEg&quot;
-                     }">
-               <button type="button" class="input-group-append input-group-text">
-                  <i id="clearIcon2" class="bi-x-lg" style="display: none;"></i>
-                  <i id="defaultClearIconToggleEg" class="bi-search" style="display: block; opacity: 1.03666;"></i>
-               </button>
-            </div>
-         </form>
+      
       </div>
    </div>
-   </div>
+</div>
 
 
    <div class="loading" wire:loading>
--- resources/views/livewire/company/suppliers/index.blade.php
+++ resources/views/livewire/company/suppliers/index.blade.php
@@ -1,54 +1,50 @@
-<div  >
-<div class="page-header  ">
-      <div class="row align-items-center ">
-         <div class="col">
- 
-         <h1 class="page-header-title pt-2">  Leveranciers  </h1>
-             </div>
- 
-         <div class="col-auto">
-         <button type="button" data-bs-toggle="modal" data-bs-target="#crudModal" class="btn btn-soft-success btn-120 " >
-            Toevoegen
-            </button>
+<div>
+   <div class="page-header  my-3">
+      <div class="row">
+         <div class="col-sm-6">
+            <h1 class="page-header-title pt-2">Locaties</h1>
          </div>
-          
-                
       </div>
    </div>
+   <div class = "filter_bar ">
 
+   <div class="row">
+  <div class="col">
+   
+       <form class = "float-start w-100">
+               <div class="input-group input-group-merge ">
+                  <input type="text" wire:model.live="filters.keyword" class="js-form-search form-control"
+                     placeholder="Zoeken op trefwoord..." data-hs-form-search-options="{
+                        &quot;clearIcon&quot;: &quot;#clearIcon2&quot;,
+                        &quot;defaultIcon&quot;: &quot;#defaultClearIconToggleEg&quot;
+                        }">
+                  <button type="button" class="input-group-append input-group-text">
+                     <i id="clearIcon2" class="bi-x-lg" style="display: none;"></i>
+                     <i id="defaultClearIconToggleEg" class="bi-search" style="display: block; opacity: 1.03666;"></i>
+                  </button>
+               </div>
+            </form> </div>  
+  <div class="col">
+   
+  <button type="button" data-bs-toggle="modal" data-bs-target="#crudModal" class="float-end btn btn-soft-success btn-120 " >
+            Toevoegen
+            </button>
+            
+         </div>  
+</div>
+
+
+
+    
+   </div>
+
+
+ 
 
    <div class="row ">
       <div class="col-xl-12">
          <div class="card  ">
-
-         <div class="card-header card-header-content-md-between bg-light">
-               <div class="mb-2 mb-md-0">
-                  <form>
-                     <!-- Search -->
-                     <div class="input-group input-group-merge">
-                        <input type="text" wire:model.live="filters.keyword" class="js-form-search form-control" placeholder="Zoeken op trefwoord..." data-hs-form-search-options="{
-                           &quot;clearIcon&quot;: &quot;#clearIcon2&quot;,
-                           &quot;defaultIcon&quot;: &quot;#defaultClearIconToggleEg&quot;
-                           }">
-                        <button type="button" class="input-group-append input-group-text">
-                        <i id="clearIcon2" class="bi-x-lg" style="display: none;"></i>
-                        <i id="defaultClearIconToggleEg" class="bi-search" style="display: block; opacity: 1.03667;"></i>
-                        </button>
-                     </div>
-                     <!-- End Search -->
-                  </form>
-               </div>
-               <div class="d-grid d-sm-flex justify-content-md-end align-items-sm-center gap-2">
-                  <div class="d-flex align-items-center justify-content-center">
-                     <div wire:loading.delay="" class="loading_indicator_small"></div>
-                  </div>
-
-
-                  <!-- End Dropdown -->
-               </div>
-            </div>
-
-
+ 
             <div class="card-body ">
                <div class="row ">
                   <div class="loading" wire:loading>
