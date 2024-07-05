67a7ee8eaadb837ecf0a6d0288da912c034c72b1
Jeff Timmer <info@lts-software.nl>
2024-07-05 20:20:19 +0200

    Update FleetManagementResource.php

--- app/Filament/Resources/FleetManagementResource.php
+++ app/Filament/Resources/FleetManagementResource.php
@@ -4,7 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\FleetManagementResource\Pages;
 use App\Filament\Resources\FleetManagementResource\RelationManagers;
-use App\Models\FleetManagement;
+use App\Models\fleetManagement;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
@@ -15,7 +15,7 @@ use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class FleetManagementResource extends Resource
 {
-    protected static ?string $model = FleetManagement::class;
+    protected static ?string $model = fleetManagement::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
     protected static ? string $navigationGroup = 'Beheer';
