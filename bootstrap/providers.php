af364111711fc3bb876ae4ebf4e030236f7f6d6b
Jeff Timmer <info@lts-software.nl>
2024-08-27 00:32:56 +0200

    Nieuw

--- bootstrap/providers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\Filament\AdminPanelProvider::class,
-    App\Providers\FortifyServiceProvider::class,
-    App\Providers\JetstreamServiceProvider::class,
-];
