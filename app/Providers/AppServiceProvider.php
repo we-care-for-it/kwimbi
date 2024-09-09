51c54318040df416ca0a2ce6de76adc13c2a7602
Jeff Timmer <info@lts-software.nl>
2024-09-10 00:04:27 +0200

    Diversen aanpassingen

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Assets\Css;
+use Filament\Facades\Filament;
+use Filament\Navigation\UserMenuItem;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -28,7 +30,27 @@ class AppServiceProvider extends ServiceProvider
             Css::make('custom-stylesheet', __DIR__ . '/../../resources/css/custom.css'),
             Css::make('custom-stylesheet', __DIR__ . '/../../resources/css/admin.css'),
         ]);
+
          
+        Filament::serving(function () {
+            Filament::registerUserMenuItems([
+                UserMenuItem::make()
+                    
+                    ->url(route('filament.admin.general'))
+                    ->icon('heroicon-o-cog-6-tooth'),
+                // ...
+            ]);
+
+            Filament::registerUserMenuItems([
+                UserMenuItem::make()
+                    
+                    ->url(route('filament.admin.general'))
+                    ->icon('heroicon-o-cog-6-tooth'),
+                // ...
+            ]);
+
+
+        });
 
         Model::unguard();
     }
