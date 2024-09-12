46c82e854aac7317e12dda1e6f11854989d0942d
Jeff Timmer <62997572+wecareforit@users.noreply.github.com>
2024-09-12 14:22:39 +0200

    Update ObjectsRelationManager.php

--- app/Filament/Resources/ObjectLocationResource/RelationManagers/ObjectsRelationManager.php
+++ app/Filament/Resources/ObjectLocationResource/RelationManagers/ObjectsRelationManager.php
@@ -2,46 +2,29 @@
 
 namespace App\Filament\Resources\ObjectLocationResource\RelationManagers;
 
-use App\Models\Elevator;
 use App\Models\ObjectMaintenanceCompany;
-use App\Models\ObjectManagementCompany;
 use App\Models\ObjectType;
-use App\Models\ProjectStatus;
-use App\Services\AddressService;
 use Filament\Forms;
-use Filament\Forms\Components\Actions\Action;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Form;
-use Filament\Forms\Get;
-use Filament\Forms\Set;
-use Filament\Notifications\Notification;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Support\Enums\Alignment;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
-
-use App\Models\ObjectLocation;
 
 
 //Form
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Textarea;
-use Filament\Forms\Components\Select;
 
 //Table
-use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Columns\ToggleColumn;
-use Filament\Tables\Columns\ImageColumn;
-use Filament\Support\Enums\MaxWidth;
-use Filament\Forms\Components\Grid;
-use Filament\Forms\Components\FileUpload;
 
 
 class ObjectsRelationManager extends RelationManager
 {
     protected static string $relationship = 'Objects';
     protected static ?string $title = 'Gekoppelde objecten';
+
     public function form(Form $form): Form
     {
         return $form
@@ -67,27 +50,18 @@ class ObjectsRelationManager extends RelationManager
                             ->maxLength(255),
 
 
-
-                        Select::make('type_id')
+                        Select::make('object_type_id')
                             ->label('Soort / Type')
-
-                    ->options(ObjectType::where('is_active',1)->pluck('name', 'id')),
-
+                            ->options(ObjectType::where('is_active', 1)->pluck('name', 'id')),
 
 
                         Select::make('maintenance_company_id')
                             ->label('Onderhoudspartij')
-
-                            ->required()
-                            ->reactive()
-                            ->options(ObjectMaintenanceCompany::all()
-                                ->pluck('name', 'id')) ,
-
-
+                            ->options(ObjectMaintenanceCompany::
+                            pluck('name', 'id')),
 
 
                     ])
-
 
 
             ]);
@@ -105,14 +79,11 @@ class ObjectsRelationManager extends RelationManager
                     ->placeholder('Geen unitnummer'),
 
                 Tables\Columns\TextColumn::make('name')->badge()
-                    ->label('Naam')->placeholder('-') ,
+                    ->label('Naam')->placeholder('-'),
 
                 Tables\Columns\TextColumn::make('nobo_no')
                     ->label('Nobonummer')->searchable()
                     ->placeholder('Geen Nobonummer'),
-
-
-
 
 
                 TextColumn::make('inspections_count')->counts('inspections')
@@ -129,12 +100,10 @@ class ObjectsRelationManager extends RelationManager
                     ->alignment(Alignment::Center),
 
 
-
                 Tables\Columns\TextColumn::make('type.name')
                     ->label('Type')->searchable()
                     ->badge()
                     ->placeholder('Onbekend'),
-
 
 
 //                Tables\Columns\TextColumn::make('location.managementcompany.name')
@@ -145,7 +114,6 @@ class ObjectsRelationManager extends RelationManager
                     ->searchable()->placeholder('Geen onderhoudspartij')
                     ->sortable()
                     ->label('Onderhoudspartij'),
-
 
 
             ])
