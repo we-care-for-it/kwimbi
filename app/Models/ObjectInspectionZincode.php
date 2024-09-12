632ec702963c2ca35aaff30468a9dc1037d2e087
Jeff Timmer <info@lts-software.nl>
2024-09-13 00:49:19 +0200

    Create ObjectInspectionZincode.php

---
+++ app/Models/ObjectInspectionZincode.php
@@ -0,0 +1,23 @@
+@@ -1,20 +0,0 @@
+<?php
+<?PHP
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
+
+use Carbon\Carbon;
+ 
+class ObjectInspectionZincode extends Model implements Auditable
+{
+    use SoftDeletes;
+    use \OwenIt\Auditing\Auditable;
+    public $table = "object_inpection_zincodes";
+    protected $fillable = ['code','description'];
+ 
+ 
+}
+
+?>
