e26c54ff5882dc95e0614905c2a004429c9c6076
Jeff Timmer <info@lts-software.nl>
2024-09-13 00:53:12 +0200

    Update toolsType.php

--- app/Models/toolsType.php
+++ app/Models/toolsType.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use OwenIt\Auditing\Contracts\Auditable;
-
+//Geen
 use Carbon\Carbon;
  
 class ToolsType extends Model implements Auditable
