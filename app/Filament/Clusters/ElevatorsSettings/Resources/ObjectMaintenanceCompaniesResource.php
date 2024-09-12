00d360a7dcc7484939332e984c728e1fbcdad2ae
Jeff Timmer <62997572+wecareforit@users.noreply.github.com>
2024-09-12 13:59:36 +0200

    Update ObjectMaintenanceCompaniesResource.php

--- app/Filament/Clusters/ElevatorsSettings/Resources/ObjectMaintenanceCompaniesResource.php
+++ app/Filament/Clusters/ElevatorsSettings/Resources/ObjectMaintenanceCompaniesResource.php
@@ -11,8 +11,6 @@ use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class ObjectMaintenanceCompaniesResource extends Resource
 {
@@ -23,122 +21,107 @@ class ObjectMaintenanceCompaniesResource extends Resource
     protected static ?string $cluster = ElevatorsSettings::class;
 
 
-    protected static ? string $navigationGroup = 'Extern';
-    protected static ? string $navigationLabel = 'Onderhoudbedrijven';
+    protected static ?string $navigationGroup = 'Extern';
+    protected static ?string $navigationLabel = 'Onderhoudbedrijven';
 
 
     public static function form(Form $form): Form
     {
         return $form
-        ->schema([
+            ->schema([
 
 
-            Forms\Components\Section::make()
-                ->schema([
+                Forms\Components\Section::make()
+                    ->schema([
 
 
-                    Forms\Components\TextInput::make('name')
-                    ->label('Naam')
-                        ->maxLength(255)
-                        ->required(),
+                        Forms\Components\TextInput::make('name')
+                            ->label('Naam')
+                            ->maxLength(255)
+                            ->required(),
 
-                    Forms\Components\TextInput::make('zipcode')
+                        Forms\Components\TextInput::make('zipcode')
+                            ->label('Postcode')
+                            ->maxLength(255),
 
-                        ->label('Postcode')
-                        ->maxLength(255),
 
-
-                    Forms\Components\TextInput::make('place')
-                    ->label('Plaats')
-                        ->maxLength(255),
+                        Forms\Components\TextInput::make('place')
+                            ->label('Plaats')
+                            ->maxLength(255),
 
 
                         Forms\Components\TextInput::make('address')
-                        ->label('Adres')
-                        ->maxLength(255),
+                            ->label('Adres')
+                            ->maxLength(255),
 
-                       // ->content(fn (Customer $record): ?string => $record->updated_at?->diffForHumans()),
-                ])
-                ->columnSpan(['lg' => 2]),
-              //  ->hidden(fn (?Customer $record) => $record === null),
-
-
-            Forms\Components\Section::make()
-                ->schema([
-
-                    Forms\Components\TextInput::make('emailaddress')
-                    ->email()
-                    ->label('E-mailadres')   ->columnSpan('full')
-
-                    ->maxLength(255),
-
-                    Forms\Components\TextInput::make('phonenumber')
-                    ->label('Telefoonnummer')   ->columnSpan('full')
-
-                    ->maxLength(255),
+                        // ->content(fn (Customer $record): ?string => $record->updated_at?->diffForHumans()),
+                    ])
+                    ->columnSpan(['lg' => 2]),
+                //  ->hidden(fn (?Customer $record) => $record === null),
 
 
+                Forms\Components\Section::make()
+                    ->schema([
 
-                ])
-                ->columns(2)
-          ->columnSpan(['lg' => 1]),
+                        Forms\Components\TextInput::make('emailaddress')
+                            ->email()
+                            ->label('E-mailadres')->columnSpan('full')
+                            ->maxLength(255),
 
-        ])
-        ->columns(3);
+                        Forms\Components\TextInput::make('phonenumber')
+                            ->label('Telefoonnummer')->columnSpan('full')
+                            ->maxLength(255),
+
+
+                    ])
+                    ->columns(2)
+                    ->columnSpan(['lg' => 1]),
+
+            ])
+            ->columns(3);
     }
 
     public static function table(Table $table): Table
     {
         return $table
+            ->columns([
+                Tables\Columns\Layout\Split::make([
+                    Tables\Columns\Layout\Stack::make([
+                        Tables\Columns\TextColumn::make('name')
+                            ->searchable()
+                            ->weight('medium')
+                            ->alignLeft()->label('Bedrijfsnaam'),
 
-        ->columns([
-            Tables\Columns\Layout\Split::make([
-                Tables\Columns\Layout\Stack::make([
-                    Tables\Columns\TextColumn::make('name')
-                        ->searchable()
+                        Tables\Columns\TextColumn::make('emailaddress')
+                            ->label('Email address')
+                            ->searchable()
+                            ->alignLeft(),
+                    ])->space(),
 
-                        ->weight('medium')
-                        ->alignLeft()        ->label('Bedrijfsnaam'),
-
-                    Tables\Columns\TextColumn::make('emailaddress')
-                        ->label('Email address')
-                        ->searchable()
-
-
-                        ->alignLeft(),
-                ])->space(),
-
-                Tables\Columns\Layout\Stack::make([
-                    Tables\Columns\TextColumn::make('address')
-                    ->searchable()
-
-                    ->weight('medium')
-                    ->alignLeft(),
+                    Tables\Columns\Layout\Stack::make([
+                        Tables\Columns\TextColumn::make('address')
+                            ->searchable()
+                            ->weight('medium')
+                            ->alignLeft(),
 
 
-
-                    Tables\Columns\TextColumn::make('zipcode')->state(
-                        function (objectMaintenanceCompany $rec) {
-                          return $rec->zipcode . " " . $rec->place;
-                         }),
-
+                        Tables\Columns\TextColumn::make('zipcode')->state(
+                            function (ObjectInspectionCompany $rec) {
+                                return $rec->zipcode . " " . $rec->place;
+                            }),
 
 
+                    ])->space(2),
 
 
-                ])->space(2),
+                    // Tables\Columns\TextColumn::make('phonenumber')
+                    // ->label('Telefoonnummer')
+                    // ->searchable()
+                    // ->sortable(),
 
 
-                // Tables\Columns\TextColumn::make('phonenumber')
-                // ->label('Telefoonnummer')
-                // ->searchable()
-                // ->sortable(),
-
-
-
-
-            ])->from('md'),
-        ])
+                ])->from('md'),
+            ])
             ->filters([
                 Tables\Filters\TrashedFilter::make(),
             ])
@@ -153,8 +136,9 @@ class ObjectMaintenanceCompaniesResource extends Resource
                     Tables\Actions\DeleteBulkAction::make()->modalHeading('Verwijderen van alle geselecteerde rijen'),
 
                 ]),
-            ]) ->emptyState(view('partials.empty-state')) ;
+            ])->emptyState(view('partials.empty-state'));
     }
+
     public static function getRelations(): array
     {
         return [
