14264db073dfcf530b24e12878ad850286aefd10
Jeff Timmer <62997572+wecareforit@users.noreply.github.com>
2024-12-12 11:15:32 +0100

    Update EditObject.php

--- app/Filament/Resources/ObjectResource/Pages/EditObject.php
+++ app/Filament/Resources/ObjectResource/Pages/EditObject.php
@@ -5,7 +5,14 @@ namespace App\Filament\Resources\ObjectResource\Pages;
 use App\Filament\Resources\ObjectResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
-
+use App\Models\ObjectLocation;
+use Filament\Forms\Components\Actions\Action;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use App\Models\Customer;
+use Filament\Support\Enums\MaxWidth;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
 class EditObject extends EditRecord
 {
     protected static string $resource = ObjectResource::class;
@@ -14,26 +21,43 @@ class EditObject extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-     
-
-
+            Actions\Action::make('updateAuthor')
+            ->fillForm(fn ($record): array => [
+                'customer_id' => $record->customer_id,
+                'address_id'  => $record->address_id,
+            ])
+            ->form([
+                Select::make('customer_id')
+                    ->label("Relatie")
+                    ->live()
+                    ->searchable()
+                    ->options(Customer::pluck('name', 'id')),
+                Select::make('address_id')
+                    ->label("Locatie")
+                    ->options(function (Get $get){
+                        return ObjectLocation::where('customer_id',$get('customer_id'))->pluck('name', 'id');
+                })
+                ->preload()
+            ])
+            ->action(function (array $data,$record): void {
+                $record->address_id  = $data['address_id'];
+                $record->customer_id = $data['customer_id'];
+                $record->save();
+            })
+                ->modalWidth(MaxWidth::Large)
+                ->modalHeading('Obejct verplaatsen')
+                ->modalDescription('Verplaatst een object naar een andere relatie en locatie.'),
             Actions\Action::make('cancel_top')
-            ->label('Afbreken')
-            ->icon('heroicon-o-arrow-uturn-left')
-            ->url($this->previousUrl ?? $this->getResource()::getUrl('index'))
-            ->iconButton(),
-
-
+                ->label('Afbreken')
+                ->icon('heroicon-o-arrow-uturn-left')
+                ->url($this->previousUrl ?? $this->getResource()::getUrl('index'))
+                ->iconButton(),
             Actions\DeleteAction::make()
-            ->iconButton()
-            ->icon('heroicon-o-trash'),
-
-            
+                ->iconButton()
+                ->icon('heroicon-o-trash'),
             Actions\Action::make('save_top')
-            ->action('save')
-            ->label('Opslaan'),
-
-
+                ->action('save')
+                ->label('Opslaan'),
         ];
     }
 
@@ -45,29 +69,22 @@ class EditObject extends EditRecord
 
       
     public function getSubheading(): ?string
-    {
-       
+    {       
         if ($this->getRecord()->location) {
-
             $location_name = NULL;
             if( $this->getRecord()->location?->name){
                 $location_name =  " | " .  $this->getRecord()->location?->name;
             }
             return   $this->getRecord()->location->address . " " . $this->getRecord()->location->zipcode . " "  . $this->getRecord()->location->place .  $location_name ;
-
-
-
-
         } else {
             return "";
         }
     
     }
-
-    
+   
     protected function getFormActions(): array
     {
-        return []; // necessary to remove the bottom actions
+        return [];
     }
     
 }
