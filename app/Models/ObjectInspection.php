b8247d66ed96704d9d4324250abbdf9b59d4fe3f
Jeff Timmer <62997572+wecareforit@users.noreply.github.com>
2025-01-14 16:57:14 +0100

    Diversen

--- app/Models/ObjectInspection.php
+++ app/Models/ObjectInspection.php
@@ -1,22 +1,13 @@
 <?php
-
 namespace App\Models;
 
-use App\Enums\QuoteTypes;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Enums\InspectionStatus;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
- 
 
-use Carbon\Carbon;
-use App\Enums\InspectionStatus;
-
-
-
-class ObjectInspection extends Model  
+class ObjectInspection extends Model
 {
     use SoftDeletes;
- 
 
     protected function casts(): array
     {
@@ -25,29 +16,24 @@ class ObjectInspection extends Model
         ];
     }
 
-
     public function elevator()
     {
-        return $this->belongsTo(Elevator::class,'nobo_number','nobo_no' );
+        return $this->belongsTo(Elevator::class, 'elevator_id', 'id');
     }
 
     public function itemdata()
     {
-        return $this->hasMany(ObjectInspectionData::class,'inspection_id','id');
+        return $this->hasMany(ObjectInspectionData::class, 'inspection_id', 'id');
     }
-
 
     public function actions()
     {
-        return $this->hasMany(systemAction::class,'item_id','id')->where('model','ObjectInspection');
+        return $this->hasMany(systemAction::class, 'item_id', 'id')->where('model', 'ObjectInspection');
     }
-
 
     public function inspectioncompany()
     {
-        return $this->belongsTo(Company::class,'inspection_company_id','id')->withTrashed();
+        return $this->belongsTo(Company::class, 'inspection_company_id', 'id')->withTrashed();
     }
-
-
 
 }
