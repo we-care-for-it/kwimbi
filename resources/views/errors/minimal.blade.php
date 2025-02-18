e886cd4abdc5b0d62a11d9eb6dba9150b00cc215
Jeff Timmer <62997572+wecareforit@users.noreply.github.com>
2025-02-18 20:51:46 +0100

    Fix composer error, add login link 404 error

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -25,7 +25,7 @@
                         Vraag de beheerder om je account toe te voegen of login met een andere account
                     </div>
 
-                    <a class = "pt-5" href = "">>Loguit</a>
+                    <a class = "pt-5" href = "/app">>Loguit</a>
                 </p>
             @endif
             <div class="w-full text-stone-300  fixed bottom-0 pb-7" >
