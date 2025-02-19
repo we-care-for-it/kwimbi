a550469da0bb24aa38970ec01698725043a0a2ec
Jeff Timmer <62997572+wecareforit@users.noreply.github.com>
2025-02-19 16:08:28 +0100

    Update 2025_02_19_131508_add_files_to_vehicles_table.php

--- database/migrations/2025_02_19_131508_add_files_to_vehicles_table.php
+++ database/migrations/2025_02_19_131508_add_files_to_vehicles_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('vehicles', function (Blueprint $table) {
-            $table->string('attachments');
+            $table->string('attachments')->nullable();
         });
     }
 
