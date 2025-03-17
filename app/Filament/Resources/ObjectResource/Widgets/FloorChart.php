9f9aafb75a3eebc6fa39924fca2047d3f2fa8d9a
Jeff Timmer <62997572+wecareforit@users.noreply.github.com>
2025-03-17 17:13:16 +0100

    Fix Floorchart

--- app/Filament/Resources/ObjectResource/Widgets/FloorChart.php
+++ app/Filament/Resources/ObjectResource/Widgets/FloorChart.php
@@ -1,10 +1,11 @@
 <?php
-
 namespace App\Filament\Resources\ObjectResource\Widgets;
 
+use App\Models\ObjectMonitoring;
 use Filament\Widgets\ChartWidget;
 use Flowframe\Trend\Trend;
 use Flowframe\Trend\TrendValue;
+use Illuminate\Database\Eloquent\Model;
 
 class FloorChart extends ChartWidget
 {
@@ -12,26 +13,24 @@ class FloorChart extends ChartWidget
     protected int|string|array $columnSpan = '7';
     protected static ?string $maxHeight        = '100%';
     public ?Model $record                      = null;
-    
-
     protected function getData(): array
     {
-        $stopData  = Trend::where('external_object_id', $this->record->monitoring_object_id)
-            ->whereYear('created_at', date('Y'))
+        $stopData = Trend::query(ObjectMonitoring::where('category', 'stop')->whereYear('created_at', date('Y'))
+                ->where('external_object_id', $this->record->monitoring_object_id)->whereYear('created_at', date('Y')))
             ->dateColumn('created_at')
             ->between(start: now()->startOfYear(), end: now()->endOfYear())
             ->perMonth()
             ->count();
 
-        $openData = Trend::where('external_object_id', $this->record->monitoring_object_id)
-            ->whereYear('created_at', date('Y'))
+        $openData = Trend::query(ObjectMonitoring::where('category', 'doors')->where('value', 1)->whereYear('created_at', date('Y'))
+                ->where('external_object_id', $this->record->monitoring_object_id)->whereYear('created_at', date('Y')))
             ->dateColumn('created_at')
             ->between(start: now()->startOfYear(), end: now()->endOfYear())
             ->perMonth()
             ->count();
 
-        $closeData = Trend::where('external_object_id', $this->record->monitoring_object_id)
-            ->whereYear('created_at', date('Y'))
+        $closeData = Trend::query(ObjectMonitoring::where('category', 'doors')->where('value', 0)->whereYear('created_at', date('Y'))
+                ->where('external_object_id', $this->record->monitoring_object_id)->whereYear('created_at', date('Y')))
             ->dateColumn('created_at')
             ->between(start: now()->startOfYear(), end: now()->endOfYear())
             ->perMonth()
@@ -40,25 +39,27 @@ class FloorChart extends ChartWidget
         return [
             'datasets' => [
                 [
-                    'label'           => 'Stop',
-                    'backgroundColor' => 'rgb(0, 0, 255)',  // Blue
-                    'borderColor'     => 'rgb(0, 0, 200)',
+                    'label'           => 'Stops',
+                    'backgroundColor' => 'rgb(194, 227, 243)',
+                    'borderColor'     => 'rgb(172, 212, 233)',
                     'data'            => $stopData->map(fn(TrendValue $value) => round($value->aggregate)),
                 ],
                 [
-                    'label'           => 'Open',
-                    'backgroundColor' => 'rgb(255, 0, 0)',  // Red
-                    'borderColor'     => 'rgb(200, 0, 0)',
+                    'label'           => 'Geopend',
+                    'backgroundColor' => 'rgb(249, 183, 196)',
+                    'borderColor'     => 'rgb(249, 161, 178)',
+
                     'data'            => $openData->map(fn(TrendValue $value) => round($value->aggregate)),
                 ],
                 [
-                    'label'           => 'Close',
-                    'backgroundColor' => 'rgb(0, 128, 0)',  // Green
-                    'borderColor'     => 'rgb(0, 100, 0)',
+                    'label'           => 'Gesloten',
+                    'backgroundColor' => 'rgb(133, 202, 143)',
+                    'borderColor'     => 'rgb(133, 202, 143)',
+
                     'data'            => $closeData->map(fn(TrendValue $value) => round($value->aggregate)),
                 ],
             ],
-            'labels' => $stopData->map(fn(TrendValue $value) => date('m', strtotime($value->date))),
+            'labels'   => $stopData->map(fn(TrendValue $value) => date('m', strtotime($value->date))),
         ];
     }
 
