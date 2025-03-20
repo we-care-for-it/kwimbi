c4e13f565a238f166e5b33adc2f84f5d15637441
Jeff Timmer <62997572+wecareforit@users.noreply.github.com>
2025-03-20 12:44:07 +0100

    Update warehouse.php

--- app/Models/warehouse.php
+++ app/Models/warehouse.php
@@ -1,10 +1,8 @@
 <?php
-
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use OwenIt\Auditing\Contracts\Auditable;
 
@@ -12,7 +10,7 @@ class Warehouse extends Model implements Auditable
 {
     use SoftDeletes;
     use \OwenIt\Auditing\Auditable;
-    
+    //rename Warehouse
     protected $fillable = ['name', 'zipcode', 'place', 'address'];
 
     public function company(): BelongsTo
