79cf36921348f98ff4dc3364fa1f3fc9cd934ee0
Jeff Timmer <62997572+wecareforit@users.noreply.github.com>
2025-03-20 12:24:38 +0100

    Update assetBrand.php

--- app/Models/assetBrand.php
+++ app/Models/assetBrand.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class AssetBrand extends Model
+class assetBrand extends Model
 {
     use HasFactory;
 
