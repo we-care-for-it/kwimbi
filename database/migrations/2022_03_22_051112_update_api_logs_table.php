d7963fc24ca0b990486f2518f73de011ec1be783
Jeff Timmer <62997572+wecareforit@users.noreply.github.com>
2025-03-20 11:27:13 +0100

    Create 2022_03_22_051112_update_api_logs_table.php

---
+++ database/migrations/2022_03_22_051112_update_api_logs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateApiLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::table('api_logs', function (Blueprint $table) {
+            $table->integer('status_code')->nullable();
+            $table->longText('payload_raw')->nullable();
+            $table->longText('response')->nullable();
+            $table->longText('response')->nullable();
+            $table->longText('response')->nullable();
+            $table->longText('response_headers')->nullable();
+            $table->text('headers')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+}
