cd0eaeb3c3ba456675e5edd93314defba670ad6d
Jeff Timmer <62997572+wecareforit@users.noreply.github.com>
2025-03-31 12:14:13 +0200

    Fix #403 Error page & Fix #410 add buttons for relation types

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,6 +2,6 @@
 
 @extends('errors::minimal')
 
-@section('title', __('Pagina niet gevonden'))
+@section('title', __('Helaas pagina is niet gevonden. Controleer het adres.'))
 @section('code', '404')
 @section('message', __('helaas de pagina die u probeert te bezoeken bestaat (niet) meer'))
--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -12,22 +12,16 @@
             <h2 class="text-center text-2xl font-bold tracking-tight pb-4">
                {{$exception?->getStatusCode()}}
             </h2>
+            @yield('title')
             <!-- <p class="  text-center ">
                @yield('message')
             </p> -->
             <p class="text-center mt-5 font-medium">
                <button  onclick="window.history.back()" class="rounded-lg bg-stone-600 hover:bg-slate-500 text-white py-2 px-4  ">Ga Terug</button>
             </p>
-            @if(Auth::user()?->companies=='[]' && $exception?->getStatusCode()=='404' )
-                <p class="text-center   mt-5 font-medium">
-                    <div class = "text-center text-stone-400 text-sm p-4 bg-stone-100">
-                        De inglogde gebruiker ({{Auth::user()->email}}) is niet gekoppeld aan een klant omgeving.<br>
-                        Vraag de beheerder om je account toe te voegen of login met een andere account
-                    </div>
-                </p>
-            @endif
-            <div class="w-full text-stone-300  fixed bottom-0 pb-7" >
-            {{$exception?->getMessage()}}
+
+            <div class="w-full text-stone-200  fixed bottom-0 pb-7" >
+            <center> {{$exception?->getMessage()}}</center>
             </div>
          </div>
       </div>
