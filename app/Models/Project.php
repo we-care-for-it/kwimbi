c560ff4eb2c85594d1a8a38118fbdab7e33b9e7e
Jeff Timmer <62997572+wecareforit@users.noreply.github.com>
2025-03-31 22:56:15 +0200

    {#411} Fix relation project dropdown

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -1,20 +1,14 @@
 <?php
-
 namespace App\Models;
 
-use Filament\Facades\Filament;
 use App\Models\Statuses;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
-
 use OwenIt\Auditing\Contracts\Auditable;
-use Log;
-use Str;
+
 class Project extends Model implements Auditable
 {
-  //  use SoftDeletes;
+    //  use SoftDeletes;
     use \OwenIt\Auditing\Auditable;
-
 
     public function location()
     {
@@ -22,38 +16,38 @@ class Project extends Model implements Auditable
     }
 
     static $rules = [
-      'name' => 'required',
-      'customer_id' => 'required',
+        'name'        => 'required',
+        'customer_id' => 'required',
     ];
 
     // Number of items to be shown per page
     protected $perPage = 20;
 
     // Attributes that should be mass-assignable
-    protected $fillable = ['slug','name','description','code','customer_id','progress','end_date','begin_date','status_id','budget_hours','budget_costs','contact_person_name'];
+    protected $fillable = ['slug', 'name', 'description', 'code', 'customer_id', 'progress', 'end_date', 'begin_date', 'status_id', 'budget_hours', 'budget_costs', 'contact_person_name'];
 
     public function status()
     {
-        return $this->hasOne(Statuses::class,'id','status_id')->where('model', 'Project');
+        return $this->hasOne(Statuses::class, 'id', 'status_id')->where('model', 'Project');
     }
 
     public function customer()
     {
-        return $this->belongsTo(Customer::class);
+        return $this->belongsTo(Relation::class, 'customer_id', 'id');
     }
-  public function uploads()
+    public function uploads()
     {
-        return $this->hasMany(Upload::class,'item_id','id');
+        return $this->hasMany(Upload::class, 'item_id', 'id');
     }
 
     public function reactions()
     {
-        return $this->hasMany(ProjectReaction::class,'project_id','id');
+        return $this->hasMany(ProjectReaction::class, 'project_id', 'id');
     }
 
     public function quotes()
     {
-        return $this->hasMany(Quote::class,'project_id','id');
+        return $this->hasMany(Quote::class, 'project_id', 'id');
     }
 
     public function company()
@@ -63,6 +57,6 @@ class Project extends Model implements Auditable
 
     public function locations()
     {
-        return $this->hasMany(ProjectLocation::class,'project_id','location_id');
+        return $this->hasMany(ProjectLocation::class, 'project_id', 'location_id');
     }
 }
