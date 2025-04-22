a5b23c6fef13728cc7f5ecee54a047f6981da016
Jeff Timmer <62997572+wecareforit@users.noreply.github.com>
2025-04-22 21:35:19 +0200

    Add location by relation

---
+++ app/Filament/Resources/RelationResource/RelationManagers/LocationsRelationManager.php
@@ -0,0 +1,268 @@
+<?php
+namespace App\Filament\Resources\RelationResource\RelationManagers;
+
+use App\Enums\LocationType;
+use App\Models\ObjectBuildingType;
+use App\Services\AddressService;
+use Filament\Forms;
+use Filament\Forms\Components\Actions\Action;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
+use Filament\Infolists\Components\Section;
+use Filament\Notifications\Notification;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Actions\RestoreAction;
+use Filament\Tables\Columns\SpatieMediaLibraryImageColumn;
+use Filament\Tables\Table;
+
+class LocationsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'locations';
+    protected static ?string $title       = 'Locaties';
+    protected static ?string $icon        = 'heroicon-o-building-office-2';
+
+    public function form(Form $form): Form
+    {
+        return $form->schema([Forms\Components\Section::make()
+                ->schema([Grid::make(2)
+                        ->schema([Forms\Components\TextInput::make("name")
+                                ->label("Naam"),
+
+                            Select::make("building_type_id")
+                                ->options(ObjectBuildingType::pluck("name", "id"))
+                                ->reactive()
+                                ->searchable()
+                                ->label("Gebouwtype"),
+
+                            Select::make("type_id")
+                                ->options(LocationType::class)
+                                ->default(1)
+                                ->reactive()
+                                ->searchable()
+                                ->label("Gebouwtype"),
+
+                        ])]),
+
+            Forms\Components\Section::make("Locatie gegevens")->schema([Grid::make(4)->schema([Forms\Components\TextInput::make("zipcode")
+                    ->label("Postcode")
+                    ->extraInputAttributes(['onInput' => 'this.value = this.value.toUpperCase()'])
+
+                    ->maxLength(255)->suffixAction(Action::make("searchAddressByZipcode")
+                        ->icon("heroicon-m-magnifying-glass")->action(function (Get $get, Set $set) {
+                        $data = (new AddressService())->GetAddress($get("zipcode"), $get("number"));
+                        $data = json_decode($data);
+
+                        if (isset($data->error_id)) {
+                            Notification::make()
+                                ->warning()
+                                ->title("Geen resultaten")
+                                ->body("Helaas er zijn geen gegevens gevonden bij de postcode <b>" . $get("zipcode") . "</b> Controleer de postcode en probeer opnieuw.")->send();
+                        } else {
+
+                            $set("place", $data?->municipality);
+                            $set("gps_lat", $data?->lat);
+                            $set("gps_lon", $data?->lng);
+                            $set("address", $data?->street);
+                            $set("municipality", $data?->municipality);
+                            $set("province", $data?->province);
+                            $set("place", $data?->settlement);
+
+                            $buildTypeExist = ObjectBuildingType::where('name', '=', $data?->purposes[0])->first();
+                            if ($buildTypeExist === null) {
+                                $buildingTypeId = ObjectBuildingType::insertGetId(['name' => ucfirst($data?->purposes[0])]);
+
+                            } else {
+                                $buildingTypeId = $buildTypeExist->id;
+                            }
+
+                            $set("building_type_id", $buildingTypeId);
+
+                        }
+                    }))->reactive(),
+
+                Forms\Components\TextInput::make("address")
+                    ->label("Straatnaam")
+                    ->required()
+                    ->columnSpan(2),
+
+                Forms\Components\TextInput::make("housenumber")
+                    ->label("Huisnummer"), Forms\Components\TextInput::make("place")
+                    ->label("Plaats"), Forms\Components\TextInput::make("province")
+                    ->label("Provincie"), Forms\Components\TextInput::make("gps_lat")
+                    ->label("GPS latitude")
+
+                    ->columnSpan(1)
+                    ->hidden(), Forms\Components\TextInput::make("gps_lon")
+                    ->label("GPS longitude")
+                    ->hidden()
+                    ->columnSpan(1),
+            ])]),
+
+            // Forms\Components\Section::make("Afbeeldingen gegevens")->collapsible()->schema([Grid::make(4)->schema([
+
+            //     SpatieMediaLibraryFileUpload::make('image')
+            //         ->multiple()
+            //         ->reorderable()
+            //         ->collection('images')
+            //         ->responsiveImages(),
+
+            // ])])
+
+            //     ->columns(2)
+            //     ->columnSpan(2),
+
+            Forms\Components\Section::make("Afbeeldingen")
+
+                ->compact()
+                ->schema([
+
+                    SpatieMediaLibraryFileUpload::make('buildingimage')
+                        ->responsiveImages()
+                        ->image()
+                        ->hiddenlabel()
+                        ->panelLayout('grid')
+                        ->maxFiles(8)
+                        ->label('Afbeeldingen')
+                        ->multiple()
+                        ->collection('locationimages'),
+
+                ])
+                ->collapsible()
+                ->collapsed(false)
+                ->persistCollapsed()
+                ->columns(1),
+
+        ])
+            ->columns(3);
+
+        Section::make()
+            ->schema([
+                Textarea::make("remark")
+                    ->rows(7)
+                    ->label("Opmerking")
+                    ->columnSpan(3)
+                    ->autosize()]);
+    }
+    public function table(Table $table): Table
+    {
+        return $table->columns(
+
+            [
+
+                Tables\Columns\TextColumn::make("type_id")
+                    ->label("Type")
+                    ->searchable()
+                    ->badge()
+                    ->width(100)
+                    ->toggleable(),
+
+                SpatieMediaLibraryImageColumn::make('locationimage')
+                    ->label('Afbeelding')
+                    ->toggleable()
+                    ->limit(2)
+                    ->placeholder("-")
+                    ->collection('locationimages'),
+
+                Tables\Columns\TextColumn::make("address")
+                    ->toggleable()
+                    ->getStateUsing(function ($record): ?string {
+                        $housenumber   = "";
+                        $complexnumber = "";
+                        $name          = "";
+                        if ($record->housenumber) {
+                            $housenumber = " " . $record->housenumber;
+                        }
+
+                        // if ($record ?->name or $record ?->complexnumber)
+                        // {
+
+                        //     return $record ?->name . " " . $complexnumber;
+                        // }
+                        // else
+                        // {
+                        return $record->address . " " . $housenumber . " - " . $record->zipcode . " - " . $record->place;
+                        // }
+                    })
+                    ->searchable()
+                    ->label("Adres")->description(function ($record) {
+
+                    $name = null;
+                    if ($record?->name) {
+                        $name = $record?->name;
+                    }
+
+                    return $name;
+
+                }),
+
+                Tables\Columns\TextColumn::make("zipcode")
+                    ->label("Postcode")
+                    ->searchable()
+                    ->toggleable()
+                    ->hidden(true),
+                Tables\Columns\TextColumn::make("place")
+                    ->label("Plaats")
+                    ->searchable()
+                    ->toggleable()
+                    ->hidden(true),
+
+                Tables\Columns\TextColumn::make("buildingtype.name")
+                    ->toggleable()
+                    ->sortable()
+                    ->label("Gebouwtype")
+                    ->badge()
+                    ->searchable()
+                    ->placeholder("Onbekend")])
+
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make()
+                    ->label('Locatie toevoegen')
+                    ->slideOver()
+                    ->modalHeading('Locatie toevoegen')
+                ,
+            ])
+            ->actions([
+
+                Tables\Actions\EditAction::make()
+                    ->label('Wijzigen')
+                    ->slideOver()
+                    ->modalHeading('Locatie wijzigen'),
+                Tables\Actions\DeleteAction::make()
+
+                    ->modalHeading('Bevestig actie')
+                    ->modalDescription('Weet je zeker dat je deze Locatie wilt verwijderen?'),
+
+                RestoreAction::make(),
+            ])
+
+        // ->bulkActions([
+
+        //     ExportBulkAction::make()
+        //         ->exports([
+        //             ExcelExport::make()
+        //                 ->fromTable()
+        //                 ->askForFilename()
+        //                 ->askForWriterType()
+        //                 ->withColumns([Column::make("place")
+        //                         ->heading("Plaats"), Column::make("address")
+        //                         ->heading("Straatnaam"), Column::make("zipcode")
+        //                         ->heading("Postcode"), Column::make("housenumber")
+        //                         ->heading("Huisnummer"), Column::make("province")
+        //                         ->heading("Provincie"), Column::make("gps_lon")
+        //                         ->heading("GPS longitude"), Column::make("gps_lat")
+        //                         ->heading("GPS latitude"), Column::make("levels")
+        //                         ->heading("Verdiepingen")])
+        //                 ->withFilename(date("m-d-Y H:i") . " - locatie export")])])
+            ->emptyState(view("partials.empty-state"));
+    }
+
+}
