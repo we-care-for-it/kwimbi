e30da7a5e778f22bffa0eee4d40563b095ba8659
Jeff Timmer <62997572+wecareforit@users.noreply.github.com>
2025-04-22 23:48:07 +0200

    Contact relation by location

---
+++ app/Filament/Resources/RelationLocationResource/Pages/ViewRelationLocation.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\RelationLocationResource\Pages;
+
+use App\Filament\Resources\RelationLocationResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewRelationLocation extends ViewRecord
+{
+    protected static string $resource = RelationLocationResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\EditAction::make(),
+        ];
+    }
+}
