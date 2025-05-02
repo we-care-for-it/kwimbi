5bd3584a5f150202e0009979ceb5e7475b0347bc
Jeff Timmer <62997572+wecareforit@users.noreply.github.com>
2025-05-02 08:52:07 +0200

    Create Employee.php

---
+++ app/Models/Employee.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
+
+class employee extends Model implements Auditable
+{
+    use SoftDeletes;
+    use \OwenIt\Auditing\Auditable;
+    //  public $table = "object_building_types";
+    //  protected $fillable = ['name', 'is_active'];
+
+    public function getNameAttribute()
+    {
+        return $this->first_name . " " . $this->last_name;
+
+    }
+    public function getAvatarAttribute($value)
+    {
+        if ($this->image) {
+            return $this->image;
+        } else {
+            return '/images/noavatar.jpg';
+        }
+    }
+
+}
