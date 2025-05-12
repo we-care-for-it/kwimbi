e6b2888d67f8d3e90ef3731b9374579a57337bc2
Jeff Timmer <62997572+wecareforit@users.noreply.github.com>
2025-05-12 11:07:47 +0200

    Add create action

--- app/Filament/Resources/ProjectsResource/Pages/ListProjects.php
+++ app/Filament/Resources/ProjectsResource/Pages/ListProjects.php
@@ -2,7 +2,9 @@
 namespace App\Filament\Resources\ProjectsResource\Pages;
 
 use App\Filament\Resources\ProjectsResource;
+use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Support\Enums\MaxWidth;
 use Relaticle\CustomFields\Filament\Tables\Concerns\InteractsWithCustomFields;
 
 class ListProjects extends ListRecords
@@ -10,17 +12,18 @@ class ListProjects extends ListRecords
     protected static string $resource = ProjectsResource::class;
     protected static ?string $title   = 'Projecten';
     use InteractsWithCustomFields;
+
     protected function getHeaderActions(): array
     {
         return [
-            // Actions\CreateAction::make()
-            //     ->modalWidth(MaxWidth::FourExtraLarge)
-            //     ->modalHeading('Project toevoegen')
-            //     ->modalDescription('Voeg een nieuwe project toe door de onderstaande gegeven zo volledig mogelijk in te vullen.')
-            //     ->icon('heroicon-m-plus')
-            //     ->modalIcon('heroicon-o-plus')
-            //     ->slideOver()
-            //     ->label('Project toevoegen'),
+            Actions\CreateAction::make()
+                ->modalWidth(MaxWidth::FourExtraLarge)
+                ->modalHeading('Project toevoegen')
+                ->modalDescription('Voeg een nieuwe project toe door de onderstaande gegeven zo volledig mogelijk in te vullen.')
+                ->icon('heroicon-m-plus')
+                ->modalIcon('heroicon-o-plus')
+                ->slideOver()
+                ->label('Project toevoegen'),
         ];
     }
     public function getHeading(): string
