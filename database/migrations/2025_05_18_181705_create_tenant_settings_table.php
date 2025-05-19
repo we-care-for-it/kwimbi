2e1dc77dbe8902a7deefa6249ccfd6d13a0f91ad
Jeff Timmer <62997572+wecareforit@users.noreply.github.com>
2025-05-19 11:43:16 +0200

    Check database exist

--- database/migrations/2025_05_18_181705_create_tenant_settings_table.php
+++ database/migrations/2025_05_18_181705_create_tenant_settings_table.php
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('settings');
+        Schema::dropIfExists('tenant_settings');
     }
 };
