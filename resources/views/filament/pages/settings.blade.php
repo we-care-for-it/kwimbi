1140e39942e02fcca1ef010e5605022b955b85ae
Jeff Timmer <62997572+wecareforit@users.noreply.github.com>
2025-05-26 10:24:06 +0200

    add settings page

--- resources/views/filament/pages/settings.blade.php
+++ resources/views/filament/pages/settings.blade.php
@@ -1,219 +1,25 @@
-<x-filament-panels::page>
-   <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
-   <div>
-      <h1 class="pb-2 text-lg font-medium text-gray-700">Mijn bedrijf</h1>
-      <div class="grid grid-cols-4 gap-4 max-xl:grid-cols-3 max-md:grid-cols-2">
+<x-filament::page>
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    <div class="space-y-6">
+        <input wire:change="search" type="text" wire:model="keyword" placeholder="Zoek naar instellingen..." class="fi-input" />
+        <div>
+            <div class="grid grid-cols-4 gap-4 max-xl:grid-cols-3 max-md:grid-cols-2">
+               @if($results->isNotEmpty())
+                  @foreach($results as $setting)
+                  <!-- /* @can($setting->permission) -->
 
-
-
-         <a class="content flex  py-5  gap-4 p-3 bg-white rounded-lg border border-gray-400  0" href="contact-types">
-            <img  src = "/images/icons/pack/conference_call.svg"   class = "max-h-12">
-            <div>
-               <h2>Contactpersonen categorieen </h2>
-               <p class="text-sm text-gray-600">Contactpersoon  types rechten instellen</p>
+                   <a class="content flex   gap-4 p-3 bg-white rounded-lg border border-gray-400  0" href="{{$setting->link}}">
+   <img  src = "/images/noimage.png"   class = "max-h-12">
+                           <div>
+                                 <h2>{{ucfirst($setting->name)}}</h2>
+                                 <p class="text-sm text-gray-600">{{ucfirst($setting->description)}}</p>
+</div>
+                        </a>
+                        <!-- @endcan -->
+                  @endforeach
+                  @else
+                  <p class="text-gray-500">Geen instellingen gevonden</p>
+               @endif
             </div>
-         </a>
-
-
-         @can('view_any_relationTypePolicy')
-         <a class="content flex  py-5  gap-4 p-3 bg-white rounded-lg border border-gray-400  0" href="relation-types">
-            <img  src = "/images/icons/pack/conference_call.svg"   class = "max-h-12">
-            <div>
-               <h2>view_any_relation::type</h2>
-               <p class="text-sm text-gray-600">Relatie types</p>
-            </div>
-         </a>
-         @endcan
-
-
-
-         @can('view_any_relation::type')
-         <a class="content flex  py-5  gap-4 p-3 bg-white rounded-lg border border-gray-400  0" href="relation-types">
-            <img  src = "/images/icons/pack/conference_call.svg"   class = "max-h-12">
-            <div>
-               <h2>Relatie types</h2>
-               <p class="text-sm text-gray-600">Locatie types</p>
-            </div>
-         </a>
-         @endcan
-         @can('view_any_relation::type')
-         <a class="content flex  py-5  gap-4 p-3 bg-white rounded-lg border border-gray-400  0" href="custom-fields">
-            <img  src = "/images/icons/pack/conference_call.svg"   class = "max-h-12">
-            <div>
-               <h2>Vrijvelden</h2>
-               <p class="text-sm text-gray-600">Wijzig en voeg vrij velden toe </p>
-            </div>
-         </a>
-         @endcan
-
-
-
-
-         @can('view_any_location::type')
-         <a class="content flex  py-5  gap-4 p-3 bg-white rounded-lg border border-gray-400  0" href="location-types">
-            <img  src = "/images/icons/pack/conference_call.svg"   class = "max-h-12">
-            <div>
-               <h2>Locatie types</h2>
-               <p class="text-sm text-gray-600">Locatie types</p>
-            </div>
-         </a>
-         @endcan
-
-
-
-         @can('view_any_object')
-         <a class="content flex  py-5  gap-4 p-3 bg-white rounded-lg border border-gray-400" href="object-monitoring-codes">
-            <img  src = "/images/icons/pack/bar_chart.svg"   class = "max-h-12">
-            <div>
-               <h2>Monitoringscodes</h2>
-               <p class="text-sm text-gray-600">Fout codes voor object monitoring</p>
-            </div>
-         </a>
-         @endcan
-      </div>
-      <h1 class="pb-2 text-lg font-medium text-gray-700 pt-10">Mijn bedrijf</h1>
-
-      <h1 class="pb-2 text-lg font-medium text-gray-700 pt-10">Assets</h1>
-      <div class="grid grid-cols-4 gap-4 max-xl:grid-cols-3 max-md:grid-cols-2">
-
-         @can('view_any_object')
-         <a class="content flex  py-5  gap-4 p-3 bg-white rounded-lg border border-gray-400" href="object-types">
-            <img  src = "/images/icons/pack/candle_sticks.svg"   class = "max-h-12">
-            <div>
-               <h2>Object types</h2>
-               <p class="text-sm text-gray-600">Object types</p>
-            </div>
-         </a>
-         @endcan
-
-         <a class="content flex  py-5  gap-4 p-3 bg-white rounded-lg border border-gray-400" href="article-category">
-            <img  src = "/images/icons/pack/candle_sticks.svg"   class = "max-h-12">
-            <div>
-               <h2>Matrieel (nog Geen rechten)</h2>
-               <p class="text-sm text-gray-600">Categorieen voor artikelen</p>
-            </div>
-         </a>
-         <a class="content flex  py-5  gap-4 p-3 bg-white rounded-lg border border-gray-400" href="article-category">
-            <img  src = "/images/icons/pack/candle_sticks.svg"   class = "max-h-12">
-            <div>
-               <h2>Matrieel statussen (nog Geen rechten)</h2>
-               <p class="text-sm text-gray-600">Categorieen voor artikelen</p>
-            </div>
-         </a>
-      </div>
-      <h1 class="pb-2 text-lg font-medium text-gray-700 pt-10">Artikelen</h1>
-      <div class="grid grid-cols-4 gap-4 max-xl:grid-cols-3 max-md:grid-cols-2">
-
-         @can('view_any_artick')
-         <a class="content flex  py-5  gap-4 p-3 bg-white rounded-lg border border-gray-400" href="article-category">
-            <img  src = "/images/icons/pack/candle_sticks.svg"   class = "max-h-12">
-            <div>
-               <h2>Categorieeen (nog Geen rechten)</h2>
-               <p class="text-sm text-gray-600">Categorieen voor artikelen</p>
-            </div>
-         </a>
-         @endcan
-      </div>
-
-
-
-      <h1 class="pb-2 text-lg font-medium text-gray-700 pt-10">Logboeken</h1>
-      <div class="grid grid-cols-4 gap-4 max-xl:grid-cols-3 max-md:grid-cols-2">
-
-      @can('view_any_authentication::log')
-         <a class="content flex  py-5  gap-4 p-3 bg-white rounded-lg border border-gray-400  0" href=" authentication-logs">
-            <img  src = "/images/icons/pack/webcam.svg"   class = "max-h-12">
-            <div>
-               <h2>Inlog logboek</h2>
-               <p class="text-sm text-gray-600">Bekijk welke gebruiker waneer is ingelogd </p>
-            </div>
-         </a>
-
-         @endcan
-
-
-
-         @can('view_any_authentication::log')
-         <a class="content flex  py-5  gap-4 p-3 bg-white rounded-lg border border-gray-400  0" href=" mail-settings">
-            <img  src = "/images/icons/pack/webcam.svg"   class = "max-h-12">
-            <div>
-               <h2>Mail instellingen</h2>
-               <p class="text-sm text-gray-600">Configureer de uitgaande mail isntellingen </p>
-            </div>
-         </a>
-
-         @endcan
-
-
-      </div>
-
-
-
-
-      <h1 class="pb-2 text-lg font-medium text-gray-700 pt-10">Werkbonnen</h1>
-      <div class="grid grid-cols-4 gap-4 max-xl:grid-cols-3 max-md:grid-cols-2">
-
-      @can('view_any_workorder::activitie')
-         <a class="content flex  py-5  gap-4 p-3 bg-white rounded-lg border border-gray-400  0" href="workorder-activities">
-            <img  src = "/images/icons/pack/webcam.svg"   class = "max-h-12">
-            <div>
-               <h2>Type werkzaamheden</h2>
-               <p class="text-sm text-gray-600">Omschrijvingen van de standaard werkzaamheden</p>
-            </div>
-         </a>
-
-         @endcan
-         @can('view_any_solution')
-         <a class="content flex  py-5  gap-4 p-3 bg-white rounded-lg border border-gray-400  0" href="/solutions">
-            <img  src = "/images/icons/pack/webcam.svg"   class = "max-h-12">
-            <div>
-               <h2>Oplossingen</h2>
-               <p class="text-sm text-gray-600">Standaard oplossingen </p>
-            </div>
-         </a>
-         @endcan
-
-         @can('view_any_error')
-         <a class="content flex  py-5  gap-4 p-3 bg-white rounded-lg border border-gray-400  0" href="/errors">
-            <img  src = "/images/icons/pack/webcam.svg"   class = "max-h-12">
-            <div>
-               <h2>Foutmeldingen</h2>
-               <p class="text-sm text-gray-600">Standaard foutmeldingen  </p>
-            </div>
-         </a>
-         @endcan
-         @can('view_any_artick')
-         <a class="content flex  py-5  gap-4 p-3 bg-white rounded-lg border border-gray-400  0" href="workorder-solution">
-            <img  src = "/images/icons/pack/biohazard.svg"   class = "max-h-12">
-            <div>
-               <h2>Storingen (nog Geen rechten)</h2>
-               <p class="text-sm text-gray-600">................</p>
-            </div>
-         </a>
-         @endcan
-      </div>
-
-NOG RECHTEN CHECKEN
-
-
-<a class="content flex  py-5  gap-4 p-3 bg-white rounded-lg border border-gray-400  0" href="brands">
-            <img  src = "/images/icons/pack/conference_call.svg"   class = "max-h-12">
-            <div>
-               <h2>Merken</h2>
-               <p class="text-sm text-gray-600">Beheer de medewerkersn</p>
-            </div>
-         </a>
-         <a class="content flex  py-5  gap-4 p-3 bg-white rounded-lg border border-gray-400  0" href="models">
-            <img  src = "/images/icons/pack/conference_call.svg"   class = "max-h-12">
-            <div>
-               <h2>Modellen</h2>
-               <p class="text-sm text-gray-600">Beheer de medewerkersn</p>
-            </div>
-         </a>
-
-
-
-
-
-   </div>
-</x-filament-panels::page>
+        </div>
+</x-filament::page>
