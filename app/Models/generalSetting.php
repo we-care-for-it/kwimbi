9c4e01021c4e9838f702f81fc7d9ef752bee19b5
Jeff Timmer <62997572+wecareforit@users.noreply.github.com>
2025-05-26 10:27:57 +0200

    Update generalSetting.php

--- app/Models/generalSetting.php
+++ app/Models/generalSetting.php
@@ -7,6 +7,6 @@ class GeneralSetting extends Model
 {
 
     // Define the table name if it's not the plural of the model name
-    protected $table = 'general_settings';
-
+    protected $table      = 'general_settings';
+    protected $connection = 'mysql';
 }
