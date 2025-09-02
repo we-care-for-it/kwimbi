31f65d822e62fd57923b14af98aba4a33a3ff33f
jtspatiebalk <j.timmer@spatiebalk.net>
2025-09-02 13:24:11 +0200

    Update PeopleRelationManager.php

--- app/Filament/Resources/RelationResource/RelationManagers/PeopleRelationManager.php
+++ app/Filament/Resources/RelationResource/RelationManagers/PeopleRelationManager.php
@@ -6,123 +6,91 @@ use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
-use App\Models\Contact;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
-
-use App\Models\Brand;
-use App\Models\Employee;
-use App\Models\ObjectModel;
-use App\Models\ObjectType;
-use App\Models\relationLocation;
-use Filament\Forms\Components\Grid;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\Textarea;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Wizard;
-use Filament\Forms\Components\Wizard\Step;
- 
 use Filament\Tables\Columns\TextColumn;
-
- 
+use Filament\Tables\Grouping\Group;
+use Filament\Forms\Components\Toggle;
 use Illuminate\Database\Eloquent\Model;
 use LaraZeus\Tiles\Tables\Columns\TileColumn;
-use Filament\Forms\Components\Checkbox;
-use Filament\Tables\Columns\ViewColumn;
-use Filament\Tables\Grouping\Group;
-use App\Filament\Exports\ObjectsExporter;
-use Filament\Tables\Actions\ExportAction;
-use Filament\Actions\Exports\Models\Export;
-use Filament\Tables\Actions\ExportBulkAction;
+use App\Models\Contact;
+use App\Models\relationLocation;
 use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Filters\SelectFilter;
-
+use Filament\Tables\Enums\FiltersLayout;
 
 class PeopleRelationManager extends RelationManager
 {
     protected static string $relationship = 'people';
-        protected static ?string $title = 'Personen';
+    protected static ?string $title = 'Personen';
     protected static ?string $modelLabel = 'Personen';
     protected static ?string $pluralModelLabel = 'Personen';
 
- 
     public static function getBadge(Model $ownerRecord, string $pageClass): ?string
     {
-        return $ownerRecord->people()   ->where('type_id', 1)      ->where('relation_id', $ownerRecord->id)->count();
+        return $ownerRecord->people()
+            ->where('type_id', 1)
+            ->where('relation_id', $ownerRecord->id)
+            ->count();
     }
-
 
     public function form(Form $form): Form
     {
-        return $form
-            ->schema([
-                Forms\Components\Select::make('type_id')
-                    ->label('Categorie / Type')
-                    ->options([
-                        1 => 'Medewerkers',
-                        2 => 'Contactpersonen',
-                    ])
-                    ->required(),
+        return $form->schema([
+            Forms\Components\Select::make('type_id')
+                ->label('Categorie / Type')
+                ->options([
+                    1 => 'Medewerker',
+                    2 => 'Contactpersoon',
+                ])
+                ->default(1)
+                ->required(),
 
-                Forms\Components\TextInput::make('first_name')
-                    ->label('Voornaam')
-                    ->required(),
+            Forms\Components\TextInput::make('first_name')
+                ->label('Voornaam')
+                ->required(),
 
-                Forms\Components\TextInput::make('last_name')
-                    ->label('Achternaam')
-                    ->required(),
+            Forms\Components\TextInput::make('last_name')
+                ->label('Achternaam')
+                ->required(),
 
-                Forms\Components\TextInput::make('email')
-                    ->label('E-mailadres')
-                    ->email(),
+            Forms\Components\TextInput::make('email')
+                ->label('E-mailadres')
+                ->email(),
 
-                Forms\Components\TextInput::make('department')
-                    ->label('Afdeling'),
+            Forms\Components\TextInput::make('department')
+                ->label('Afdeling'),
 
-                Forms\Components\TextInput::make('function')
-                    ->label('Functie'),
+            Forms\Components\TextInput::make('function')
+                ->label('Functie'),
 
-                Forms\Components\TextInput::make('phone_number')
-                    ->label('Telefoonnummer')
-                    ->tel()
-                    ->regex('/^\+?\d{6,20}$/'),
+            Forms\Components\TextInput::make('phone_number')
+                ->label('Telefoonnummer')
+                ->tel()
+                ->regex('/^\+?\d{6,20}$/'),
 
-                Forms\Components\Select::make('location_id')
-                    ->label('Locatie')
-                    ->options(fn() => relationLocation::pluck('address', 'id')),
+            Forms\Components\Select::make('location_id')
+                ->label('Locatie')
+                ->options(fn() => relationLocation::pluck('address', 'id')),
 
-                Forms\Components\Toggle::make('show_in_contactlist')
-                    ->label('Toon in contactpersonen overzicht'),
-            ]);
+            Toggle::make('show_in_contactlist')
+                ->label('Toon in contactpersonen overzicht'),
+        ]);
     }
 
     public function table(Table $table): Table
     {
         return $table
-
             ->defaultGroup('type_id')
-          ->groups([
-   
-                            Group::make('type_id')
-                               ->titlePrefixedWithLabel(true)
-                                ->getTitleFromRecordUsing(fn($record): string => match($record->type_id) {
-            1 => 'Medewerker',
-            2 => 'Contactpersoon',
-            default => 'Onbekend',
-        })
-                ->label('Categorie')
-              
-                  ,
-
- 
-
-        ])   
-
-
-
-
-           // ->groupBy(fn($record) => $record->type_id == 1 ? 'Medewerkers' : 'Contactpersonen')
+            ->groups([
+                Group::make('type_id')
+                    ->titlePrefixedWithLabel(true)
+                    ->getTitleFromRecordUsing(fn($record): string => match ($record->type_id) {
+                        1 => 'Medewerker',
+                        2 => 'Contactpersoon',
+                        default => 'Onbekend',
+                    })
+                    ->label('Categorie'),
+            ])
             ->columns([
                 TileColumn::make('name')
                     ->description(fn($record) => $record->function)
@@ -161,29 +129,32 @@ class PeopleRelationManager extends RelationManager
                     ->sortable(),
             ])
             ->filters([
-                  TrashedFilter::make(),
-                // SelectFilter::make('department')
-                //     ->label('Afdeling')
-                //     ->options(fn() => Contact::select('department')->distinct()->pluck('department')),
+                TrashedFilter::make(),
                 SelectFilter::make('type_id')
                     ->label('Categorie')
                     ->options([
                         1 => 'Medewerkers',
                         2 => 'Contactpersonen',
                     ]),
-            ])
+            ], layout: FiltersLayout::Modal)
             ->headerActions([
                 Tables\Actions\CreateAction::make()
-                   ->slideOver(),
+                    ->label('Persoon toevoegen')
+                    ->slideOver(),
             ])
             ->actions([
+                Tables\Actions\Action::make('openObject')
+                    ->icon('heroicon-m-eye')
+                    ->url(fn($record) => route('filament.app.resources.tickets.view', ['record' => $record]))
+                    ->label('Bekijk'),
+
                 Tables\Actions\EditAction::make()
-                ->slideOver(),
-                Tables\Actions\DeleteAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
+                    ->label('Snel bewerken')
+                    ->slideover(),
+
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\DeleteAction::make()
+                        ->label('Verwijder'),
                 ]),
             ]);
     }
